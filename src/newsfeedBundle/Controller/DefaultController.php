6544020f6b57a7534a0ddb26d2f8aa990911ec60
Gavin Moulton <gavin.moulton@gmail.com>
2017-11-30 08:49:14 +0000

    move to helper functions complete db insert code

--- src/newsfeedBundle/Controller/DefaultController.php
+++ src/newsfeedBundle/Controller/DefaultController.php
@@ -2,122 +2,161 @@
 
 namespace newsfeedBundle\Controller;
 
-
+use newsfeedBundle\Helper\ApiConnect;
+use newsfeedBundle\Helper\DomCrawler;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\DomCrawler\Crawler;
-use GuzzleHttp\Client;
+
 
 /* @author Gavin Moulton
  *
- * date
-title
-summary
-url
+ *
+ * TODO add summary details
 */
 
 class DefaultController extends Controller
 {
-    private $listing;
 
     /**
      * @Route("/", name="homepage")
      */
-
     public function indexAction() {
 
         // hacker news
-        $dataHackerNews = $this->feedAction('https://hacker-news.firebaseio.com/v0/topstories.json','json');
-
+        if(ApiConnect::feed('https://hacker-news.firebaseio.com/v0/topstories.json','json') === '404') {
+            echo 'hacker news feed failed to connect';
+        } else {
+            $dataHackerNews = ApiConnect::feed('https://hacker-news.firebaseio.com/v0/topstories.json','json');
+        }
         $data = json_decode($dataHackerNews);
-
+        // get 10 of 100 records
         $data = array_slice($data,0,10);
 
         foreach($data as $item) {
 
-        $content = $this->feedAction('https://hacker-news.firebaseio.com/v0/item/' . $item .'.json','json');
+            if (ApiConnect::feed('https://hacker-news.firebaseio.com/v0/item/' . $item . '.json','json') === '404') {
+                echo 'hacker news feed item failed to connect';
+            } else {
+                $content = ApiConnect::feed('https://hacker-news.firebaseio.com/v0/item/' . $item . '.json', 'json');
 
-        $dataContent = json_decode($content,true);
+                $dataContent = json_decode($content, true);
 
-        $sql = "INSERT into feed(date,title,url) VALUES (?,?,?)";
+                try {
 
-        $em = $this->getDoctrine()->getManager();
+                    $em = $this->getDoctrine()->getManager();
 
-        $connection = $em->getConnection();
+                    $connection = $em->getConnection();
 
-            $statement = $connection->prepare($sql);
+                    $sql = "INSERT into feed(date,title,url) VALUES (?,?,?)";
 
-            $currentTime = \DateTime::createFromFormat('U',$dataContent['time']);
+                    $statement = $connection->prepare($sql);
 
-            $dateIs = $currentTime->format('Y-m-d H:m:i');
+                    $currentTime = \DateTime::createFromFormat('U', $dataContent['time']);
 
-            $statement->bindParam(1, $dateIs);
-            $statement->bindParam(2,$dataContent['title']);
-            //$statement->bindParam(3,'summary");
-            $statement->bindParam(3,$dataContent["url"]);
+                    $dateIs = $currentTime->format('Y-m-d H:m:i');
 
-            $statement->execute();
+                    $statement->bindParam(1, $dateIs);
+                    $statement->bindParam(2, $dataContent['title']);
+                    //$statement->bindParam(3,'summary");
+                    $statement->bindParam(3, $dataContent["url"]);
+
+                    $statement->execute();
+
+                } catch (\Exception $e) {
+                    error_log($e->getMessage());
+                }
+            }
 
         }
-
         // bbc
-        $bbc = $this->feedAction('http://feeds.bbci.co.uk/news/technology/rss.xml','xml');
+        if(ApiConnect::feed('http://feeds.bbci.co.uk/news/technology/rss.xml','xml') === '404') {
+            echo 'bbc feed no connection';
+        } else {
 
-        $bbc = new \SimpleXMLElement($bbc,true);
+            $bbc = ApiConnect::feed('http://feeds.bbci.co.uk/news/technology/rss.xml', 'xml');
 
-        $bbcContents = json_decode(json_encode($bbc),true);
+            $bbc = new \SimpleXMLElement($bbc, true);
+            // turn into json then array for easy handling
+            $bbcContents = json_decode(json_encode($bbc), true);
 
-        foreach ($bbcContents['channel']['item'] as $bbcContent) {
+            foreach ($bbcContents['channel']['item'] as $bbcContent) {
 
-         // echo $bbcContent['link'];
-        //  echo $bbcContent['pubDate'];
-         // get title from content
+                try {
+
+                    $title = "mocktitle";
+                    $em = $this->getDoctrine()->getManager();
+
+                    $connection = $em->getConnection();
+
+                    $sql = "INSERT into feed(date,title,url) VALUES (?,?,?)";
+
+                    $statement = $connection->prepare($sql);
+
+                    $currentTime = new \DateTime($bbcContent['pubDate']);
+
+                    $dateIs = $currentTime->format('Y-m-d H:m:i');
+
+                    $statement->bindParam(1, $dateIs);
+                    $statement->bindParam(2, $title);
+                    //$statement->bindParam(3,'bbc');
+                    $statement->bindParam(3, $bbcContent['link']);
+
+                    $statement->execute();
+
+                } catch (\Exception $e) {
+                    error_log($e->getMessage());
+                }
+
+            }
 
         }
 
         // slashddot
-        $slashDot = $this->feedAction('https://slashdot.org','dom');
-
-        $slashNews = $this->domAction($slashDot);
-
-        return 'yes';
-    }
-
-    public function feedAction($url,$type)
-    {
-        $source = new Client();
-
-        $feed = $source->get($url);
-
-        if($feed->getStatusCode() == '200') {
-
-            if($type == 'xml') {
-                $this->listing = $feed->getBody()->getContents();
-            } else {
-                $this->listing = $feed->getBody()->getContents();
-            }
-            return $this->listing;
-           // return $this->render('newsfeedBundle:Default:index.html.twig', array('listing' => $this->listing));
-
+        if(ApiConnect::feed('https://slashdot.org', 'dom') === '404') {
+            echo 'no slashdot feed connection';
         } else {
 
-            return new Response('Sorry, Listings unavailable');
+            $slashNews = ApiConnect::feed('https://slashdot.org', 'dom');
+            $slashResults = DomCrawler::domAction($slashNews);
+
+
+            try {
+
+                foreach($slashResults as $result) {
+
+                    $date = new \DateTime();
+
+                    // issues getting any more values in dom
+                    $mockdateis = $date->format('Y-m-d H:m:i');
+                    $mocktitleis = 'slashdot';
+                    $mockurlis = 'url';
+
+                    $em = $this->getDoctrine()->getManager();
+
+                    $connection = $em->getConnection();
+
+                    $sql = "INSERT into feed(date,title,url) VALUES (?,?,?)";
+
+                    $statement = $connection->prepare($sql);
+
+                    $statement->bindParam(1, $mockdateis);
+                    $statement->bindParam(2, $result);
+                    //$statement->bindParam(3,'summary");
+                    $statement->bindParam(3,$mockurlis);
+
+                    $statement->execute();
+
+
+                }
+
+            } catch (\Exception $e) {
+                error_log($e->getMessage());
+            }
 
         }
 
-    }
-
-    public function domAction($content) {
-
-     $crawler = new Crawler($content);
-
-     $crawler->filterXPath('//div[@id="firehose"]//h2')->each(function (Crawler $node) use (&$results) {
-
-     var_dump(trim($node->text()));
-
-     });
-
+        return new Response('Data processing complete');
 
     }
 
